9534e6102fab13329796a5ac9708971ee594a4c5
vinilsikhapalli <vsikhapalli@nisum.com>
2021-01-06 10:03:05 +0530

    custom directive init

---
+++ directives/src/components/customButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div >
+    <p>onclick custom</p>
+    <button v-customClick="clicked"> click here for custom button </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customButton',
+  directives:{
+    customClick:{
+      bind(el,binding){
+        el.onclick = function(){
+          binding.value();
+        }
+      }
+    }
+  },
+  methods:{
+    clicked(){
+      alert(' hai i am from custom event')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
